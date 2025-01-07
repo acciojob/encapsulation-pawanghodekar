6b3a574474781215efa0f78e6d241010e62f140a
pavan <pawan.fbypa19@sinhgad.edu>
2025-01-07 11:10:51 +0530

    Encapsulation

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+
+        // Setting the value of name using the setter
+        obj.setName("John");
+
+        // Accessing the value of name using the getter
+        System.out.println("The name is: " + obj.getName());
+    }
 }
